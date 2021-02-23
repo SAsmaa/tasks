fb6cc5ab3268639b430730fb22ead81736923f65
SAsmaa <asoomsalman@gmail.com>
2021-02-23 18:27:01 +0200

    initial commit

---
+++ app/Models/task.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class task extends Model
+{
+    protected $fillable = ['title', 'description','par_cat_id'];
+}
---
+++ database/migrations/2021_02_22_193538_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('row_id')->nullable(false);
+            $table->string('title')->nullable(false);
+            $table->string('description')->nullable(true);
+            $table->date('due_date')->nullable(false);
+            $table->string('tags')->nullable(true);
+            $table->integer('par_cat_id')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
